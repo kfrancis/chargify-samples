efd74498b04fdb166f7acf6f9ddfd8ffc3072c4e
Kori Francis <byter@me.com>
2012-11-14 02:11:34 -0500

    fix menus, start on local signup model

---
+++ Source/NET/Chargify.MVC4/Controllers/SignupController.cs
@@ -0,0 +1,38 @@
+﻿using Chargify.MVC4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Chargify.MVC4.Controllers
+{
+    public class SignupController : Controller
+    {
+        //
+        // GET: /Signup/Local
+
+        public ActionResult Local()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Signup/Local
+
+        [HttpPost]
+        public ActionResult Local(LocalSignup model)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Source/NET/Chargify.MVC4/Models/SignupModels.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Chargify.MVC4.Models
+{
+    public class LocalSignup
+    {
+        public RegisterModel User { get; set; }
+
+        public UserContactModel UserContact { get; set; }
+
+        public BillingAddressModel UserAddress { get; set; }
+
+        public BillingPaymentModel UserPayment { get; set; }
+
+        [Display(Name = "Coupon Code")]
+        public string CouponCode { get; set; }
+    }
+
+    public class UserContactModel
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email")]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+    }
+
+    public class BillingAddressModel
+    {
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string State { get; set; }
+
+        [Required]
+        public string Country { get; set; }
+
+        [Required]
+        public string Zip { get; set; }
+    }
+
+    public class BillingPaymentModel
+    {
+        [Required]
+        [Display(Name = "Exp. Month")]
+        public int ExpirationMonth { get; set; }
+
+        [Required]
+        [Display(Name = "Exp. Year")]
+        public int ExpirationYear { get; set; }
+
+        [Required]
+        [Display(Name = "Card Number")]
+        public string CardNumber { get; set; }
+
+        [Required]
+        public string CVV { get; set; }
+    }
+
+}
